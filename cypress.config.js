6d52e9834594ed448348f99920d46f6cfcc8f49c
pertsatiy <pertsatiy@gmail.com>
2022-08-09 23:26:11 +0300

    sda
    asd

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,7 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
+module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
-      // inside config.browsers array each object has information like
-      // {
-      //   name: 'chrome',
-      //   channel: 'canary',
-      //   family: 'chromium',
-      //   displayName: 'Canary',
-      //   version: '80.0.3966.0',
-      //   path:
-      //    '/Applications/Canary.app/Contents/MacOS/Canary',
-      //   majorVersion: 80
-      // }
-      return {
-        browsers: config.browsers.filter(
-          (b) => b.family === 'chromium' && b.name !== 'electron'
-        ),
-      }
-    }
-  }
-})
+      // implement node event listeners here
+    },
+  },
+};
