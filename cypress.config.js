acaf300389d26416b6a677c5eb2c635c821bff76
pertsatiy <pertsatiy@gmail.com>
2022-08-10 01:23:59 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,7 @@ module.exports = defineConfig({
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
+    embeddedScreenshots: true
     //     embeddedScreenshots: true
   },
   e2e: {
