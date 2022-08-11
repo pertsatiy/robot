3175cd7952aadf470171ae815e1fa62dce573ff0
pertsatiy <pertsatiy@gmail.com>
2022-08-11 14:43:11 +0300

    Update robots.cy.js
    bug

--- cypress/e2e/moincoins/robots.cy.js
+++ cypress/e2e/moincoins/robots.cy.js
@@ -13,7 +13,7 @@ describe("Moincoins", ()=>{
             expect(response.status).to.eq(200);
             expect(response.body).to.have.not.null;
             expect(response.body).to.be.a('string');
-            expect(response.body).to.eq('User-agent: *\n\nDisallow: */trafficback$\nDisallow: */callback$\nDisallow: */my-account\nDisallow: */mein-konto\nDisallow: */user-delete/*\nDisallow: */verification/*\nDisallow: */reset-password/*\nSitemap: https://moincoins.com/sitemap_index.xml');
+            expect(response.body).to.eq('Usrer-agent: *\n\nDisallow: */trafficback$\nDisallow: */callback$\nDisallow: */my-account\nDisallow: */mein-konto\nDisallow: */user-delete/*\nDisallow: */verification/*\nDisallow: */reset-password/*\nSitemap: https://moincoins.com/sitemap_index.xml');
 
           })
         })
