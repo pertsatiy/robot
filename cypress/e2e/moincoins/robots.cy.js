7837307b5dc93ef616d0ef8589d53ba5aabf83ab
pertsatiy <pertsatiy@gmail.com>
2022-08-11 12:05:10 +0300

    Update robots.cy.js
    статус 200 все гуд

--- cypress/e2e/moincoins/robots.cy.js
+++ cypress/e2e/moincoins/robots.cy.js
@@ -10,7 +10,7 @@ describe("Moincoins", ()=>{
           })
           
           .then((response) => {
-            expect(response.status).to.eq(404);
+            expect(response.status).to.eq(200);
             expect(response.body).to.have.not.null;
             expect(response.body).to.be.a('string');
             expect(response.body).to.eq('User-agent: *\n\nDisallow: */trafficback$\nDisallow: */callback$\nDisallow: */my-account\nDisallow: */mein-konto\nDisallow: */user-delete/*\nDisallow: */verification/*\nDisallow: */reset-password/*\nSitemap: https://moincoins.com/sitemap_index.xml');
